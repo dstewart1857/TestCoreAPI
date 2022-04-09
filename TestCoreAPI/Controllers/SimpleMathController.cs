c18372f9dbed37434b85443a6ae007ece78bf470
audiomojo <32555462+audiomojo@users.noreply.github.com>
2022-04-08 23:37:28 -0600

    Added SimpleMath endpoint for first problem

---
+++ TestCoreAPI/Controllers/SimpleMathController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestCoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleMathController : Controller
+    {
+        [HttpGet(Name = "simpleMath")]
+        public String simpleMath(int operandOne, int operandTwo, char operation)
+        {
+            int result = operandOne + operandTwo;
+
+            return result.ToString();
+        }
+    }
+}
