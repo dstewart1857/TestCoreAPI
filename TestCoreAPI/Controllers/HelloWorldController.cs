2422cf338ce727d145ce21b2b66073fa29a6764a
audiomojo <32555462+audiomojo@users.noreply.github.com>
2022-04-08 23:25:31 -0600

    Add project files.

---
+++ TestCoreAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestCoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet(Name = "helloWorld")]
+        public String helloWorld(String name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
