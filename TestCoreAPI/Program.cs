909c6b79999c42e5b1593e277c528b6d1b736224
audiomojo <32555462+audiomojo@users.noreply.github.com>
2022-05-08 21:44:55 -0600

    Added static functionality

--- TestCoreAPI/Program.cs
+++ TestCoreAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseFileServer(new FileServerOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "Static")),
+    RequestPath = "/Static"
+});
 
 app.UseHttpsRedirection();
 
